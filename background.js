fd6bb8b32c675c380c2b499d5a38f805c1a471c0
zxyvip911126-create <zxyvip911126@gmail.com>
2025-12-20 05:54:18 +0800

    删除 background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if (details.url.includes("京东广告")) {
-            return { cancel: true };  // 阻止广告请求
-        }
-    },
-    { urls: ["*://*.jd.com/*"] },
-    ["blocking"]
-);

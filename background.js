a5366985d7b2a84bb0605ca135da108b28de2344
zxyvip911126-create <zxyvip911126@gmail.com>
2025-12-20 05:50:45 +0800

    更新 background.js

--- background.js
+++ background.js
@@ -1,2 +1,9 @@
-// 目前不需要额外的背景任务，空白即可
-console.log("JD Express Ads Remover extension is running.");
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if (details.url.includes("京东广告")) {
+            return { cancel: true };  // 阻止广告请求
+        }
+    },
+    { urls: ["*://*.jd.com/*"] },
+    ["blocking"]
+);

8a5c44f9996d1e0838270bad8519f125412d96ea
zxyvip911126-create <zxyvip911126@gmail.com>
2025-12-20 05:38:39 +0800

    更新 background.js

--- background.js
+++ background.js
@@ -1,11 +1,2 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    // 在这里通过正则匹配广告的请求并取消它
-    if (details.url.indexOf('https://jd.com/ad') !== -1) {
-      return { cancel: true }; // 取消广告请求
-    }
-    return {};
-  },
-  { urls: ["*://*.jd.com/*"] }, // 监听所有JD的URL
-  ["blocking"]
-);
+// 目前不需要额外的背景任务，空白即可
+console.log("JD Express Ads Remover extension is running.");

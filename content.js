1c0100ce650df3144660aeacdc4667e3e7b39e80
zxyvip911126-create <zxyvip911126@gmail.com>
2025-12-20 06:19:59 +0800

    删除 content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-  // 假设广告元素有类名 'ad-banner'，你可以根据实际情况修改
-  const adElement = document.querySelector('.ad-banner');
-  if (adElement) {
-    adElement.style.display = 'none';  // 隐藏广告
-    console.log("Ad element removed");
-  }
-});
